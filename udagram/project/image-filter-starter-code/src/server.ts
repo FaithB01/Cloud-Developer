428a7c2e11e828b9fd3d01606b357d5e40e7f14c
FaithB01 <fnbiketi@gmail.com>
2022-07-07 18:08:08 +0300

    updated gitgnore

--- udagram/project/image-filter-starter-code/src/server.ts
+++ udagram/project/image-filter-starter-code/src/server.ts
@@ -1,16 +1,42 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { IndexRouter } from './controllers/index.router';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
+
+  // Init the Express application
   const app = express();
-  const port = process.env.PORT || 8080; // default port to listen
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
   
+  // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO
-  // We are using the index router as a way to organize better the code and not let this file too long.
-  app.use('/', IndexRouter)
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
 
   // Start the Server
   app.listen( port, () => {
--- udagram/project/image-filter-starter-code/src/util/util.ts
+++ udagram/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import Jimp = require('jimp');
+import fs from "fs";
+import Jimp = require("jimp");
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,17 +9,22 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
-    return new Promise(async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
-        await photo
-            .resize(256, 256) // resize
-            .quality(60) // set JPEG quality
-            .greyscale() // set greyscale
-            .write(__dirname + outpath, (img) => {
-                resolve(__dirname + outpath);
-            });
-    });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const photo = await Jimp.read(inputURL);
+      const outpath =
+        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      await photo
+        .resize(256, 256) // resize
+        .quality(60) // set JPEG quality
+        .greyscale() // set greyscale
+        .write(__dirname + outpath, (img) => {
+          resolve(__dirname + outpath);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
 // deleteLocalFiles
@@ -28,16 +33,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
 export async function deleteLocalFiles(files: Array<string>) {
-    for (let file of files) {
-        fs.unlinkSync(file);
-    }
-}
-
-//Local Context to instanciate local images
-export class LocalContext {
-    filePath: string
-
-    constructor(filePath: string) {
-        this.filePath = filePath;
-    }
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
 }
